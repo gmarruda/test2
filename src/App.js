5ffd5ea0e7bbc7bc16c731a2b0d15e55128d0fa2
Gustavo Migliorini Arruda <gmarruda@gmail.com>
2025-02-17 10:31:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('https://test3-gustavocc.purplemoss-92287ba4.westeurope.azurecontainerapps.io', { name });
+      const response = await axios.post('https://apigustavocc.azure-api.net/test3/', { name });
       setMessage(`Response: ${response.data.message}`);
     } catch (error) {
       console.error('Error posting name:', error);
